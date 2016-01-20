0f17db1d5657db14f994a5c11fb61786db3438a7
Emerson Mendonça <Emerson Mendona>
2016-01-20 09:07:08 -0200

    email bug corrigido

--- includes/contato.php
+++ includes/contato.php
@@ -1,22 +1,14 @@
-
-    <?php
-        /*
-        error_reporting(0);
-        ini_set('display_errors', 0);
-
-        $nome = $_POST['nome'];
-        $email = $_POST['email'];
-        $empresa = $_POST['empresa'];
-        $msg = $_POST['msg'];
-        
-        $to = 'emerson.mdca@gmail.com';
-        $subj = 'Nova Mensagem do Site emersonmendonca.com';
-        //$msg= wordwrap($msg, 70);
-        $msg .= '/n $nome, $email, $empresa';
-        
-        mail($to, $subj, $msg);
-        */
-    ?>
+<?php
+    error_reporting(0);
+    ini_set('display_errors', 0);
+    
+    $msg = 'NOME: $nome <br />, E-MAIL: $email <br />, EMPRESA: $empresa <br /> <br /> Mensagem: <br />';
+    
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $empresa = $_POST['empresa'];
+    $msg .= $_POST['msg'];
+?>
 
 <form id="contato" action="index.php#panel-3" method="post">
 	<!--<label for="nome">Nome</label>-->
@@ -48,8 +40,13 @@
     </style>
     
     <?php
-    
         if(!empty($nome)){
+            $to = 'emerson.mdca@gmail.com';
+            $subj = 'Nova Mensagem do Site emersonmendonca.com';
+            //$msg= wordwrap($msg, 70);
+            
+            mail($to, $subj, $msg);
+           
             echo    '<div id="success-msg">
                     <span class="glyphicon glyphicon-ok"></span>
                     <p id="msg" >Mensagem enviada com sucesso ;)</p>
